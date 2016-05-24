f6b8affc27900510ed318efcbd50dadb11bfb394
dzirt <ihopeyoudie@mai.ru>
2016-05-24 10:37:29 +0400

    Add print utils

--- src/net/dzirt/Main.java
+++ src/net/dzirt/Main.java
@@ -1,9 +1,18 @@
 package net.dzirt;
+import static net.dzirt.print.Print.*;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello ;dfio;sog");
+        System.out.println("Привет, сегодня: ");
+        System.out.println(new Date());
+        print();
+        printnb("jjjjj   ");
+        print("pijipjp");
+
+
     }
 }
---
+++ src/net/dzirt/print/Print.java
@@ -0,0 +1,20 @@
+package net.dzirt.print;
+
+//import java.util.Objects;
+//import java.io.*;
+
+/**
+ * Created by Андрей on 24.05.2016.
+ */
+public class Print {
+    public static void print(Object obj){
+        System.out.println(obj);
+    }
+    public static void print(){
+        System.out.println();
+    }
+    public static void printnb(Object obj){
+        System.out.print(obj);
+    }
+
+}
